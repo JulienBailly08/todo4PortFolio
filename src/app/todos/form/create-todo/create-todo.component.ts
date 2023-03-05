904d2b0480f70ecea47648ac88da27213dae3477
Juju on sbire Volant <julienbailly08@gmail.com>
2023-03-05 16:09:44 +0100

    creation des formulaires et front OK

--- src/app/todos/form/create-todo/create-todo.component.ts
+++ src/app/todos/form/create-todo/create-todo.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Todo } from 'src/app/models/todo';
+import { Router } from '@angular/router';
+import { TodoService } from 'src/app/services/todo.service';
 
 @Component({
   selector: 'app-create-todo',
@@ -7,9 +11,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CreateTodoComponent implements OnInit {
 
-  constructor() { }
+  @Input()
+  todo4Form!: Todo
+  @Output() updateSucess = new EventEmitter<string>();
+  response$:any;
+  erreurPourInfo = null;
+  todoForm!: FormGroup;
 
+  constructor(private fb: FormBuilder, private todoService: TodoService, private router: Router) { }
+  
   ngOnInit(): void {
+    this.todoForm = this.fb.group({
+      title: ['',Validators.required],
+      content: ['', [Validators.required, Validators.minLength(8)]],
+    })
+  }
+
+  onSubmit()
+  {
+    this.updateSucess.emit();
+  }
+  
+  get title() {
+    return this.todoForm.get('title');
+  }
+  get content() {
+    return this.todoForm.get('content');
   }
 
 }
